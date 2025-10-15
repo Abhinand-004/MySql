cc647ca32d6c02998234f19f03ee6f8115625009
Abhinand S <abhinandkarikkakom@gmail.com>
2025-10-15 16:46:18 +0530

    hw1

--- hw.py
+++ hw.py
@@ -1,3 +1,8 @@
 rice=45;
 sug=40;
 oil=130;
+cust_rice=3;
+cust_sug=2.5;
+cust_oil=1.8;
+cus_tot=(cust_rice*rice)+(cust_sug*sug)+(cust_oil*oil);
+print("Total cost of your purchase is:"+str(cus_tot));
